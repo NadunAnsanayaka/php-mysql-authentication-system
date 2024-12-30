e1dc76835484e9c9206663b19819ee9f734a2d82
Nadun Ansanayaka <nadunansanayaka@gmail.com>
2024-12-30 15:18:33 +0530

    create signup.inc.php

---
+++ includes/signup.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+    $pwdRepeat = $_POST["pwdrepeat"];
+
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+
+    $emptyInputs = isEmptyInputSignup($name,$email,$username, $pwd, $pwdRepeat);
+    $invalidUid = invalidUid($username);
+    $invalidEmail = invalidEmail($email);
+    $pwdMatch = pwdMatch($pwd,$pwdRepeat);
+    $uidExists = uidExists($conn,$username, $email);
+    
+ 
+    if($emptyInputs !== false){
+        header("Location: ../signup.php?error=emptyinput");
+        exit();
+    }
+    if($invalidUid !== false){
+        header("Location: ../signup.php?error=invalidUid");
+        exit();
+    }
+    if($invalidEmail !== false){
+        header("Location: ../signup.php?error=invalidEmail");
+        exit();
+    }
+    if($pwdMatch !== false){
+        header("Location: ../signup.php?error=pwdnotmatch");
+        exit();
+    } 
+    if($uidExists !== false){
+        header("Location: ../signup.php?error=thisOneTaken");
+        exit();
+    }
+    
+   creatUsers($conn, $name, $email, $username, $pwd);
+
+} else {
+    header('Location:../login.php');
+    exit();
+}
