16c45217c292910cb2a07fa69a022316d7ba610f
Nadun Ansanayaka <nadunansanayaka@gmail.com>
2024-12-30 15:18:04 +0530

    create login.inc.php

---
+++ includes/login.inc.php
@@ -0,0 +1,19 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $username = $_POST["uid"];
+    $pwd = $_POST["pwd"];
+
+    require_once 'dbh.inc.php';
+    require_once 'functions.inc.php';
+
+    if(isEmptyInputLogin($username,$pwd) !== false){
+        exit();
+    }
+    
+    loginUser($conn,$username,$pwd);
+
+} else {
+    header('Location:../login.php');
+    exit();
+}
